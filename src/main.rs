2dd2ec613900894a6ccf7cfc6ffcf0dfa872410c
Eden Reich <eden.reich@gmail.com>
2024-03-14 21:16:15 +0100

    Don't start the operator if the CRD's are not installed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use env_logger::Env;
+use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
 use k8s_operator::watch_resource;
 use kube::{
-    api::{Api, WatchParams},
+    api::{Api, ListParams, WatchParams},
     Client,
 };
-use log::info;
+use log::{error, info};
 use tokio::time::{sleep, Duration};
+
+async fn check_any_crd_from_group(client: Client, group: &str) -> anyhow::Result<bool> {
+    let crds: Api<CustomResourceDefinition> = Api::all(client);
+    let lp = ListParams::default();
+    let crd_list = crds.list(&lp).await?;
+
+    Ok(crd_list.items.iter().any(|crd| crd.spec.group == group))
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -17,6 +26,11 @@ async fn main() -> anyhow::Result<()> {
     let client = Client::try_default().await?;
 
     let watch_params = WatchParams::default().timeout(10);
+
+    if !check_any_crd_from_group(client.clone(), "example.com").await? {
+        error!("No CRD's found from the group. Please install the CRD's first. Exiting...");
+        return Ok(());
+    }
 
     tokio::spawn(watch_resource::<k8s_operator::Cat>(
         Api::default_namespaced(client.clone()).clone(),
