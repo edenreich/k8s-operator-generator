f405aff5be93e39c460766cb0fd0b49acf1a3f38
Eden Reich <eden.reich@gmail.com>
2024-03-16 15:52:22 +0100

    Refactor the code a bit

--- src/controllers/cat.rs
+++ src/controllers/cat.rs
@@ -3,45 +3,52 @@ use crate::{add_finalizer, remove_finalizer};
 use kube::api::{Api, WatchEvent};
 use kube::Resource;
 use log::info;
+use pets_api_client::apis::default_api::DefaultApi;
 
-pub async fn handle_cat(event: WatchEvent<Cat>, api: Api<Cat>) {
-    match event {
+pub async fn handle_cat(event: WatchEvent<Cat>, api: Api<Cat>, client: &DefaultApi) {
+    let kind = Cat::kind(&());
+    let kind_str = kind.to_string();
+
+    let (mut cat, event_type) = match event {
         WatchEvent::Added(mut cat) => {
-            if cat.metadata.deletion_timestamp.is_some() {
-                info!(
-                    "{} Sending API call to delete the remote resource and wait for response: {:?}",
-                    "Cat", cat.metadata.name
-                );
-                remove_finalizer(&mut cat, api.clone()).await;
-            } else {
+            if cat.metadata.deletion_timestamp.is_none() {
                 add_finalizer(&mut cat, api.clone()).await;
-                add_event(Cat::kind(&()).to_string(), &mut cat, "Added".to_string()).await;
-                info!(
-                    "{} Added: {:?} {:?}",
-                    "Cat", cat.metadata.name, cat.metadata.finalizers
-                )
+                client.create_cat(&cat).await;
+            } else {
+                client.delete_cat(&cat).await;
+                remove_finalizer(&mut cat, api.clone()).await;
             }
+            (cat, "Added")
         }
-        WatchEvent::Modified(cat) => {
-            info!(
-                "{} Modified: {:?} {:?}",
-                "Cat", cat.metadata.name, cat.metadata.finalizers
-            );
+        WatchEvent::Modified(mut cat) => {
+            client.update_cat(&cat).await;
+            (cat, "Modified")
         }
-        WatchEvent::Deleted(cat) => {
-            info!(
-                "{} Deleted: {:?} {:?}",
-                "Cat", cat.metadata.name, cat.metadata.finalizers
-            );
+        WatchEvent::Deleted(mut cat) => {
+            client.delete_cat(&cat).await;
+            (cat, "Deleted")
         }
         WatchEvent::Bookmark(bookmark) => {
-            info!(
-                "{} Bookmark: {:?}",
-                "Cat", bookmark.metadata.resource_version
-            );
+            info!("Cat Bookmark: {:?}", bookmark.metadata.resource_version);
+            return;
         }
         _ => {
-            info!("{} Unknown event {:?}", "Cat", event);
+            info!("Cat Unknown event {:?}", event);
+            return;
         }
-    }
+    };
+
+    add_event(
+        kind_str.clone(),
+        &mut cat,
+        event_type.into(),
+        kind_str.clone(),
+        format!("Cat Resource {} Remotely", event_type),
+    )
+    .await;
+
+    info!(
+        "Cat {}: {:?} {:?}",
+        event_type, cat.metadata.name, cat.metadata.finalizers
+    );
 }
