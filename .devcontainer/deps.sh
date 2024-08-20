b4242f82f1a534cd0758c6141c5c20e69d24ab56
Eden Reich <eden.reich@gmail.com>
2024-08-20 14:31:11 +0000

    Add missing deps for the build

--- .devcontainer/deps.sh
+++ .devcontainer/deps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/zsh
 
+sudo apt-get update && sudo apt-get install -y pkg-config libssl-dev
+
 # Install favorite prompt theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
