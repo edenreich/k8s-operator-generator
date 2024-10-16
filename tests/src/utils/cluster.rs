16cca633f8b4e0c5119d62548f46e8dc71d2d297
Eden Reich <eden.reich@gmail.com>
2024-10-16 13:59:25 +0000

    Fix the pipeline analyse job error

--- tests/src/utils/cluster.rs
+++ tests/src/utils/cluster.rs
@@ -4,7 +4,7 @@ pub async fn setup() -> anyhow::Result<()> {
     let root_dir = Path::new("..");
     tokio::process::Command::new("task")
         .arg("cluster-create")
-        .current_dir(&root_dir)
+        .current_dir(root_dir)
         .status()
         .await?;
     Ok(())
@@ -14,7 +14,7 @@ pub async fn teardown() -> anyhow::Result<()> {
     let root_dir = Path::new("..");
     let _ = tokio::process::Command::new("task")
         .arg("cluster-delete")
-        .current_dir(&root_dir)
+        .current_dir(root_dir)
         .status()
         .await?;
     Ok(())
--- tests/src/utils/operator.rs
+++ tests/src/utils/operator.rs
@@ -5,13 +5,13 @@ pub async fn deploy() -> anyhow::Result<(), anyhow::Error> {
     let root_dir = Path::new("..");
     let _ = tokio::process::Command::new("task")
         .arg("package")
-        .current_dir(&root_dir)
+        .current_dir(root_dir)
         .status()
         .await?;
 
     let mut child = tokio::process::Command::new("task")
         .arg("deploy-operator")
-        .current_dir(&root_dir)
+        .current_dir(root_dir)
         .stdout(std::process::Stdio::piped())
         .stderr(std::process::Stdio::piped())
         .spawn()
