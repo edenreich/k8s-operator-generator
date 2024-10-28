47a23611d1424da9517d5e32fee68e48d28cd6a1
Eden Reich <eden.reich@gmail.com>
2024-10-28 18:10:12 +0100

    chore(release): Bump the version of the cli to v1.0.0

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -21,7 +21,7 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(
     name = "Kubernetes Operator Codegen",
-    version = "1.0",
+    version = "v1.0.0",
     author = "Eden Reich <eden.reich@gmail.com>",
     arg_required_else_help = true
 )]
