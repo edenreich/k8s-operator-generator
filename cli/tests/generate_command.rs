7d03d8c13be7c6f71c84d8fe36da228a93a53a6c
Eden Reich <eden.reich@gmail.com>
2024-11-17 15:55:37 +0000

    test: Refactor tests, ecapsulate them into their own module
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- cli/tests/generate_command.rs
+++ cli/tests/generate_command.rs
@@ -1,18 +1,21 @@
 mod utils;
 
-use kopgen::{
-    commands::generate::{execute, generate_types},
-    errors::AppError,
-    utils::read_openapi_spec,
-};
-use openapiv3::{OpenAPI, Schema};
-use std::{collections::HashMap, fs};
-use tempfile::tempdir;
-use utils::create_temp_file;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use kopgen::{
+        commands::generate::{execute, generate_types},
+        errors::AppError,
+        utils::read_openapi_spec,
+    };
+    use openapiv3::{OpenAPI, Schema};
+    use std::{collections::HashMap, fs};
+    use tempfile::tempdir;
+    use utils::create_temp_file;
 
-#[test]
-fn test_execute_fails_because_of_missing_kubernetes_extension() -> Result<(), AppError> {
-    let openapi_yaml = r#"---
+    #[test]
+    fn test_execute_fails_because_of_missing_kubernetes_extension() -> Result<(), AppError> {
+        let openapi_yaml = r#"---
 openapi: 3.0.0
 info:
   title: Test API
@@ -22,137 +25,137 @@ components:
   schemas: {} 
 "#;
 
-    let (dir, openapi_file) = create_temp_file("openapi.yaml", openapi_yaml);
+        let (dir, openapi_file) = create_temp_file("openapi.yaml", openapi_yaml);
 
-    let result = execute(
-        &dir.path().to_string_lossy().to_string(),
-        &openapi_file,
-        &false,
-        &false,
-        &false,
-        &false,
-        &false,
-    );
+        let result = execute(
+            &dir.path().to_string_lossy().to_string(),
+            &openapi_file,
+            &false,
+            &false,
+            &false,
+            &false,
+            &false,
+        );
 
-    assert!(
-        result.is_err(),
-        "Expected execution to fail due to missing Kubernetes extension."
-    );
+        assert!(
+            result.is_err(),
+            "Expected execution to fail due to missing Kubernetes extension."
+        );
 
-    match result {
-        Err(AppError::MissingRequiredExtension(ref key)) => {
-            assert_eq!(
-                key, "x-kubernetes-operator-group",
-                "Expected missing extension key to be 'x-kubernetes-operator-group'."
-            );
-        }
-        Err(e) => {
-            panic!("Expected MissingRequiredExtension error, but got: {:?}", e);
-        }
-        Ok(_) => {
-            panic!(
+        match result {
+            Err(AppError::MissingRequiredExtension(ref key)) => {
+                assert_eq!(
+                    key, "x-kubernetes-operator-group",
+                    "Expected missing extension key to be 'x-kubernetes-operator-group'."
+                );
+            }
+            Err(e) => {
+                panic!("Expected MissingRequiredExtension error, but got: {:?}", e);
+            }
+            Ok(_) => {
+                panic!(
                 "Expected execution to fail due to missing Kubernetes extension, but it succeeded."
             );
+            }
         }
+
+        Ok(())
     }
 
-    Ok(())
-}
+    /// This test need to be fixed.
 
-/// This test need to be fixed.
+    // #[test]
+    // fn test_execute_success() -> Result<(), AppError> {
+    //     let openapi_yaml = r#"---
+    // openapi: 3.0.0
+    // info:
+    //   title: Test API
+    //   version: 1.0.0
+    //   x-kubernetes-operator-group: testgroup
+    //   x-kubernetes-operator-version: v1
+    //   x-kubernetes-operator-resource-ref: uuid
+    //   x-kubernetes-operator-status-ref: status
+    //   x-kubernetes-operator-include-tags:
+    //     - test
+    //     - example
+    //   x-kubernetes-operator-example-metadata-spec-field-ref: metadata
+    // paths: {}
+    // components:
+    //   schemas: {}
+    // "#;
+    //     let (_dir, openapi_file) = create_temp_file("openapi.yaml", openapi_yaml);
 
-// #[test]
-// fn test_execute_success() -> Result<(), AppError> {
-//     let openapi_yaml = r#"---
-// openapi: 3.0.0
-// info:
-//   title: Test API
-//   version: 1.0.0
-//   x-kubernetes-operator-group: testgroup
-//   x-kubernetes-operator-version: v1
-//   x-kubernetes-operator-resource-ref: uuid
-//   x-kubernetes-operator-status-ref: status
-//   x-kubernetes-operator-include-tags:
-//     - test
-//     - example
-//   x-kubernetes-operator-example-metadata-spec-field-ref: metadata
-// paths: {}
-// components:
-//   schemas: {}
-// "#;
-//     let (_dir, openapi_file) = create_temp_file("openapi.yaml", openapi_yaml);
+    //     let all = true;
+    //     let lib = false;
+    //     let manifests = false;
+    //     let controllers = false;
+    //     let types = false;
 
-//     let all = true;
-//     let lib = false;
-//     let manifests = false;
-//     let controllers = false;
-//     let types = false;
+    //     // TODO - pass to this function a base directory to write the generated files and use relative paths
+    //     let result = execute(&openapi_file, &all, &lib, &manifests, &controllers, &types)?;
 
-//     // TODO - pass to this function a base directory to write the generated files and use relative paths
-//     let result = execute(&openapi_file, &all, &lib, &manifests, &controllers, &types)?;
+    //     // assert!(result.is_ok());
 
-//     // assert!(result.is_ok());
+    //     Ok(())
+    // }
 
-//     Ok(())
-// }
+    #[test]
+    fn test_execute_missing_openapi_file() -> Result<(), AppError> {
+        let dir = tempdir().unwrap();
+        let openapi_file = dir.path().join("missing_openapi.yaml");
 
-#[test]
-fn test_execute_missing_openapi_file() -> Result<(), AppError> {
-    let dir = tempdir().unwrap();
-    let openapi_file = dir.path().join("missing_openapi.yaml");
+        let all = true;
+        let lib = false;
+        let manifests = false;
+        let controllers = false;
+        let types = false;
 
-    let all = true;
-    let lib = false;
-    let manifests = false;
-    let controllers = false;
-    let types = false;
+        let result = execute(
+            &dir.path().to_string_lossy().to_string(),
+            &openapi_file.to_string_lossy().to_string(),
+            &all,
+            &lib,
+            &manifests,
+            &controllers,
+            &types,
+        );
 
-    let result = execute(
-        &dir.path().to_string_lossy().to_string(),
-        &openapi_file.to_string_lossy().to_string(),
-        &all,
-        &lib,
-        &manifests,
-        &controllers,
-        &types,
-    );
+        assert!(result.is_err());
 
-    assert!(result.is_err());
+        Ok(())
+    }
 
-    Ok(())
-}
-
-#[test]
-fn test_execute_invalid_openapi_spec() -> Result<(), AppError> {
-    let openapi_yaml = r#"---
+    #[test]
+    fn test_execute_invalid_openapi_spec() -> Result<(), AppError> {
+        let openapi_yaml = r#"---
 invalid_yaml: [unclosed_list
 "#;
-    let (dir, openapi_file) = create_temp_file("invalid_openapi.yaml", openapi_yaml);
+        let (dir, openapi_file) = create_temp_file("invalid_openapi.yaml", openapi_yaml);
 
-    let all = true;
-    let lib = false;
-    let manifests = false;
-    let controllers = false;
-    let types = false;
+        let all = true;
+        let lib = false;
+        let manifests = false;
+        let controllers = false;
+        let types = false;
 
-    let result = execute(
-        &dir.path().to_string_lossy().to_string(),
-        &openapi_file,
-        &all,
-        &lib,
-        &manifests,
-        &controllers,
-        &types,
-    );
+        let result = execute(
+            &dir.path().to_string_lossy().to_string(),
+            &openapi_file,
+            &all,
+            &lib,
+            &manifests,
+            &controllers,
+            &types,
+        );
 
-    assert!(result.is_err());
+        assert!(result.is_err());
 
-    Ok(())
-}
+        Ok(())
+    }
 
-#[test]
-fn test_generate_types() -> Result<(), AppError> {
-    let openapi_yaml = r#"
+    #[test]
+    fn test_generate_types() -> Result<(), AppError> {
+        let openapi_yaml = r#"
 openapi: 3.0.0
 info:
   title: Test API
@@ -179,42 +182,43 @@ components:
         - id
 "#;
 
-    let (dir, openapi_file_path) = create_temp_file("openapi.yaml", openapi_yaml);
-    let output_path: std::path::PathBuf = dir.path().join("src").join("types");
-    fs::create_dir_all(&output_path).unwrap();
+        let (dir, openapi_file_path) = create_temp_file("openapi.yaml", openapi_yaml);
+        let output_path: std::path::PathBuf = dir.path().join("src").join("types");
+        fs::create_dir_all(&output_path).unwrap();
 
-    let openapi: OpenAPI = read_openapi_spec(openapi_file_path.as_str())?;
+        let openapi: OpenAPI = read_openapi_spec(openapi_file_path.as_str())?;
 
-    let schemas: HashMap<String, Schema> = openapi
-        .components
-        .ok_or_else(|| AppError::Other("No components found in OpenAPI spec".to_string()))?
-        .schemas
-        .iter()
-        .filter_map(|(name, schema)| match schema {
-            openapiv3::ReferenceOr::Item(schema) => Some((name.clone(), schema.clone())),
-            openapiv3::ReferenceOr::Reference { .. } => None,
-        })
-        .collect();
+        let schemas: HashMap<String, Schema> = openapi
+            .components
+            .ok_or_else(|| AppError::Other("No components found in OpenAPI spec".to_string()))?
+            .schemas
+            .iter()
+            .filter_map(|(name, schema)| match schema {
+                openapiv3::ReferenceOr::Item(schema) => Some((name.clone(), schema.clone())),
+                openapiv3::ReferenceOr::Reference { .. } => None,
+            })
+            .collect();
 
-    let operator_resource_ref = "id".to_string();
-    let types_directory = output_path
-        .to_str()
-        .expect("Failed to convert output path to string");
+        let operator_resource_ref = "id".to_string();
+        let types_directory = output_path
+            .to_str()
+            .expect("Failed to convert output path to string");
 
-    let result = generate_types(types_directory, &schemas, &operator_resource_ref);
+        let result = generate_types(types_directory, &schemas, &operator_resource_ref);
 
-    assert!(result.is_ok());
+        assert!(result.is_ok());
 
-    let type_file = output_path.join("user.rs");
+        let type_file = output_path.join("user.rs");
 
-    assert!(type_file.exists(), "Type file was not created");
+        assert!(type_file.exists(), "Type file was not created");
 
-    let generated_content = fs::read_to_string(&type_file)?;
-    assert!(
-        generated_content.contains("struct User"),
-        "Generated content does not contain expected struct"
-    );
+        let generated_content = fs::read_to_string(&type_file)?;
+        assert!(
+            generated_content.contains("struct User"),
+            "Generated content does not contain expected struct"
+        );
 
-    dir.close()?;
-    Ok(())
+        dir.close()?;
+        Ok(())
+    }
 }
--- cli/tests/init_command.rs
+++ cli/tests/init_command.rs
@@ -1,179 +1,182 @@
-use assert_cmd::Command;
-use predicates::prelude::*;
-use serial_test::serial;
-use std::{env, fs};
-use tempfile::tempdir;
+#[cfg(test)]
+mod tests {
+    use assert_cmd::Command;
+    use predicates::prelude::*;
+    use serial_test::serial;
+    use std::{env, fs};
+    use tempfile::tempdir;
 
-fn setup_env() {
-    env::set_var(
-        "TARGET_ARCH",
-        format!("{}-unknown-linux-musl", std::env::consts::ARCH),
-    );
-    env::set_var(
-        "OPENAPI_DOWNLOAD_URL",
-        "https://github.com/edenreich/kopgen/blob/main/openapi.yaml",
-    );
-    env::set_var("INSTALL_CRDS", "true");
-    env::set_var("RUST_LOG", "info");
-    env::set_var("RUST_BACKTRACE", "1");
-    env::set_var("CONTAINER_REGISTRY", "localhost:5005");
-    env::set_var("CLUSTER_NAME", "k3d-k3s-default");
-    env::set_var("RELEASE", "false");
-    env::set_var("KUBERNETES_OPERATOR_GROUP", "test-group");
-    env::set_var("KUBERNETES_OPERATOR_VERSION", "v1");
-    env::set_var("KUBERNETES_OPERATOR_RESOURCE_REF", "test-resource-ref");
-    env::set_var("KUBERNETES_OPERATOR_INCLUDE_TAGS", "tag1,tag2");
-    env::set_var(
-        "KUBERNETES_OPERATOR_EXAMPLE_METADATA_SPEC_FIELD_REF",
-        "test-field-ref",
-    );
-}
-
-fn clear_env() {
-    env::remove_var("OPENAPI_DOWNLOAD_URL");
-    env::remove_var("INSTALL_CRDS");
-    env::remove_var("RUST_LOG");
-    env::remove_var("RUST_BACKTRACE");
-    env::remove_var("CONTAINER_REGISTRY");
-    env::remove_var("CLUSTER_NAME");
-    env::remove_var("TARGET_ARCH");
-    env::remove_var("RELEASE");
-    env::remove_var("KUBERNETES_OPERATOR_GROUP");
-    env::remove_var("KUBERNETES_OPERATOR_VERSION");
-    env::remove_var("KUBERNETES_OPERATOR_RESOURCE_REF");
-    env::remove_var("KUBERNETES_OPERATOR_INCLUDE_TAGS");
-    env::remove_var("KUBERNETES_OPERATOR_EXAMPLE_METADATA_SPEC_FIELD_REF");
-}
-
-#[test]
-#[serial]
-fn init_creates_required_directories() {
-    setup_env();
-
-    let temp_dir = tempdir().unwrap();
-    let path = temp_dir.path();
-
-    // Run the `init` command using the CLI
-    Command::cargo_bin("kopgen")
-        .unwrap()
-        .arg("init")
-        .arg(path.to_str().unwrap())
-        .assert()
-        .success()
-        .stderr(predicate::str::contains("Initialized project at")); // TODO - investigate why the env_logger by default all goes to stderr, it's weird.
-
-    // Check the expected directories exist
-    let expected_dirs = [
-        ".cargo",
-        ".devcontainer",
-        "operator",
-        "crdgen",
-        "crdgen/src",
-        "tests",
-        "tests/src/utils",
-        "operator/src/controllers",
-        "operator/src/types",
-        "manifests/crds",
-        "manifests/rbac",
-        "manifests/operator",
-        "manifests/examples",
-    ];
-
-    for dir in &expected_dirs {
-        assert!(path.join(dir).exists(), "Directory {} was not created", dir);
-    }
-}
-
-#[test]
-#[serial]
-fn init_creates_required_files() {
-    setup_env();
-
-    let temp_dir = tempdir().unwrap();
-    let path = temp_dir.path();
-
-    // Run the `init` command
-    Command::cargo_bin("kopgen")
-        .unwrap()
-        .arg("init")
-        .arg(path)
-        .assert()
-        .success();
-
-    // Check the expected files exist
-    let expected_files = [
-        ".openapi-generator-ignore",
-        "Cargo.toml",
-        ".dockerignore",
-        ".editorconfig",
-        ".env.example",
-        ".gitattributes",
-        ".gitignore",
-        "Taskfile.yml",
-        "README.md",
-        ".prettierrc.yaml",
-        ".rustfmt.toml",
-        "Cluster.yaml",
-        "Dockerfile",
-        "operator/Cargo.toml",
-        "crdgen/Cargo.toml",
-        "tests/Cargo.toml",
-        ".devcontainer/devcontainer.json",
-        ".devcontainer/deps.sh",
-        ".devcontainer/setup-git.sh",
-        ".devcontainer/launch.json",
-        ".devcontainer/.zshrc",
-        ".cargo/config.toml",
-        "operator/src/main.rs",
-        "operator/src/cli.rs",
-        "operator/src/controllers/mod.rs",
-        "operator/src/types/mod.rs",
-        "tests/src/utils/mod.rs",
-        "tests/src/utils/client.rs",
-        "tests/src/utils/operator.rs",
-        "tests/src/utils/cluster.rs",
-        "tests/main.rs",
-    ];
-
-    for file in expected_files {
-        let file_path = path.join(file);
-        assert!(
-            file_path.exists(),
-            "File {} was not created",
-            file_path.display()
+    fn setup_env() {
+        env::set_var(
+            "TARGET_ARCH",
+            format!("{}-unknown-linux-musl", std::env::consts::ARCH),
+        );
+        env::set_var(
+            "OPENAPI_DOWNLOAD_URL",
+            "https://github.com/edenreich/kopgen/blob/main/openapi.yaml",
+        );
+        env::set_var("INSTALL_CRDS", "true");
+        env::set_var("RUST_LOG", "info");
+        env::set_var("RUST_BACKTRACE", "1");
+        env::set_var("CONTAINER_REGISTRY", "localhost:5005");
+        env::set_var("CLUSTER_NAME", "k3d-k3s-default");
+        env::set_var("RELEASE", "false");
+        env::set_var("KUBERNETES_OPERATOR_GROUP", "test-group");
+        env::set_var("KUBERNETES_OPERATOR_VERSION", "v1");
+        env::set_var("KUBERNETES_OPERATOR_RESOURCE_REF", "test-resource-ref");
+        env::set_var("KUBERNETES_OPERATOR_INCLUDE_TAGS", "tag1,tag2");
+        env::set_var(
+            "KUBERNETES_OPERATOR_EXAMPLE_METADATA_SPEC_FIELD_REF",
+            "test-field-ref",
         );
     }
 
-    clear_env();
-}
+    fn clear_env() {
+        env::remove_var("OPENAPI_DOWNLOAD_URL");
+        env::remove_var("INSTALL_CRDS");
+        env::remove_var("RUST_LOG");
+        env::remove_var("RUST_BACKTRACE");
+        env::remove_var("CONTAINER_REGISTRY");
+        env::remove_var("CLUSTER_NAME");
+        env::remove_var("TARGET_ARCH");
+        env::remove_var("RELEASE");
+        env::remove_var("KUBERNETES_OPERATOR_GROUP");
+        env::remove_var("KUBERNETES_OPERATOR_VERSION");
+        env::remove_var("KUBERNETES_OPERATOR_RESOURCE_REF");
+        env::remove_var("KUBERNETES_OPERATOR_INCLUDE_TAGS");
+        env::remove_var("KUBERNETES_OPERATOR_EXAMPLE_METADATA_SPEC_FIELD_REF");
+    }
 
-#[test]
-#[serial]
-fn init_skips_existing_directory() {
-    setup_env();
+    #[test]
+    #[serial]
+    fn init_creates_required_directories() {
+        setup_env();
 
-    let temp_dir = tempdir().unwrap();
-    let path = temp_dir.path();
+        let temp_dir = tempdir().unwrap();
+        let path = temp_dir.path();
 
-    // Pre-create the directory to simulate an existing path
-    fs::create_dir_all(path).unwrap();
+        // Run the `init` command using the CLI
+        Command::cargo_bin("kopgen")
+            .unwrap()
+            .arg("init")
+            .arg(path.to_str().unwrap())
+            .assert()
+            .success()
+            .stderr(predicate::str::contains("Initialized project at")); // TODO - investigate why the env_logger by default all goes to stderr, it's weird.
 
-    // Place a file in the directory to ensure it's not removed
-    fs::write(path.join("existing-file.txt"), "Hello, world!").unwrap();
+        // Check the expected directories exist
+        let expected_dirs = [
+            ".cargo",
+            ".devcontainer",
+            "operator",
+            "crdgen",
+            "crdgen/src",
+            "tests",
+            "tests/src/utils",
+            "operator/src/controllers",
+            "operator/src/types",
+            "manifests/crds",
+            "manifests/rbac",
+            "manifests/operator",
+            "manifests/examples",
+        ];
 
-    // Run the `init` command
-    Command::cargo_bin("kopgen")
-        .unwrap()
-        .arg("init")
-        .arg(path)
-        .assert()
-        .success()
-        .stderr(predicate::str::contains(
-            "Directory already exists and is not empty",
-        ));
+        for dir in &expected_dirs {
+            assert!(path.join(dir).exists(), "Directory {} was not created", dir);
+        }
+    }
 
-    // Verify no other files are created
-    assert_eq!(fs::read_dir(path).unwrap().count(), 1);
+    #[test]
+    #[serial]
+    fn init_creates_required_files() {
+        setup_env();
 
-    clear_env();
+        let temp_dir = tempdir().unwrap();
+        let path = temp_dir.path();
+
+        // Run the `init` command
+        Command::cargo_bin("kopgen")
+            .unwrap()
+            .arg("init")
+            .arg(path)
+            .assert()
+            .success();
+
+        // Check the expected files exist
+        let expected_files = [
+            ".openapi-generator-ignore",
+            "Cargo.toml",
+            ".dockerignore",
+            ".editorconfig",
+            ".env.example",
+            ".gitattributes",
+            ".gitignore",
+            "Taskfile.yml",
+            "README.md",
+            ".prettierrc.yaml",
+            ".rustfmt.toml",
+            "Cluster.yaml",
+            "Dockerfile",
+            "operator/Cargo.toml",
+            "crdgen/Cargo.toml",
+            "tests/Cargo.toml",
+            ".devcontainer/devcontainer.json",
+            ".devcontainer/deps.sh",
+            ".devcontainer/setup-git.sh",
+            ".devcontainer/launch.json",
+            ".devcontainer/.zshrc",
+            ".cargo/config.toml",
+            "operator/src/main.rs",
+            "operator/src/cli.rs",
+            "operator/src/controllers/mod.rs",
+            "operator/src/types/mod.rs",
+            "tests/src/utils/mod.rs",
+            "tests/src/utils/client.rs",
+            "tests/src/utils/operator.rs",
+            "tests/src/utils/cluster.rs",
+            "tests/main.rs",
+        ];
+
+        for file in expected_files {
+            let file_path = path.join(file);
+            assert!(
+                file_path.exists(),
+                "File {} was not created",
+                file_path.display()
+            );
+        }
+
+        clear_env();
+    }
+
+    #[test]
+    #[serial]
+    fn init_skips_existing_directory() {
+        setup_env();
+
+        let temp_dir = tempdir().unwrap();
+        let path = temp_dir.path();
+
+        // Pre-create the directory to simulate an existing path
+        fs::create_dir_all(path).unwrap();
+
+        // Place a file in the directory to ensure it's not removed
+        fs::write(path.join("existing-file.txt"), "Hello, world!").unwrap();
+
+        // Run the `init` command
+        Command::cargo_bin("kopgen")
+            .unwrap()
+            .arg("init")
+            .arg(path)
+            .assert()
+            .success()
+            .stderr(predicate::str::contains(
+                "Directory already exists and is not empty",
+            ));
+
+        // Verify no other files are created
+        assert_eq!(fs::read_dir(path).unwrap().count(), 1);
+
+        clear_env();
+    }
 }
