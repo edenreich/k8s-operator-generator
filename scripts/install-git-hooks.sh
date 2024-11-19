512a1640f65c7fbb52bc699a3c458f62b896eb37
Eden Reich <eden.reich@gmail.com>
2024-11-19 14:39:59 +0000

    feat(hooks): Add pre-commit hook for linting, static analysis, and testing
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

---
+++ scripts/install-git-hooks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+HOOK_DIR=".git/hooks"
+HOOK_FILE="pre-commit"
+
+echo "Installing pre-commit hook..."
+
+# Copy the pre-commit file
+cp ./hooks/$HOOK_FILE $HOOK_DIR/$HOOK_FILE
+chmod +x $HOOK_DIR/$HOOK_FILE
+
+echo "Pre-commit hook installed."
